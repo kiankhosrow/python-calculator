58c7191c2353496ecd88a2a9fdd0be1a8d7c8f90
FarDin-FaHim <fardinfahim1@gmail.com>
2022-12-02 21:50:21 +0100

    Add Update

--- test.py
+++ test.py
@@ -2,20 +2,14 @@ import tkinter
 from tkinter import *
 
 win = Tk()
-win.title("Simple Calculator- FahimF")
+
+win.title("This is just for test")
 win.geometry("570x600+100+200")
 win.resizable(False, False)
-win.configure(bg="#17161b")
+win.configure(bg="red")
 
-label_result = Label(win, width=25, height=2,
-                     text="", font=("arial", 30))
-label_result.pack()
-
-# First Row.....
-Button(win, text="AC", width=5, height=1, font=("ariall", 30, "bold"),
-       bd=10, fg="#fff", bg="red").place(x=10, y=100)
-Button(win, text="/", width=5, height=1, font=("ariall", 30, "bold"),
-       bd=8, fg="#fff", bg="#2a2d36").place(x=150, y=100)
+Button(win, text="Fardin", width=2, height=1,
+       bd=1, bg="#2a2d36").place(x=100, y=200)
 
 
 win.mainloop()
